47267eb33942190e082bfed815036d62c4b2c74c
gpinchon <pinchon.guillaume@gmail.com>
2022-07-12 23:44:04 +0200

    add vectors to Config file

--- include/Config.hpp
+++ include/Config.hpp
@@ -2,74 +2,101 @@
 
 #include <filesystem>
 #include <map>
+#include <vector>
 #include <variant>
+#include <algorithm>
 
 /**
 * This is the common configuration between APIs
 */
 namespace Config {
-class File {
-public:
-    /**
-     * @brief Loads the Config from the specified file, invalid values will be ignored (set to default value) but setting keys will still be registered
-     * @param path the absolute path to the config file
-     */
-    void Parse(const std::filesystem::path path);
-    /**
-     * @brief Saves this Config to the specified file
-     * @param path the absolute path to the file this Config is to be saved to
-     */
-    void Save(const std::filesystem::path path);
-    /**
-     * @brief Tries to get the specified setting, set it to the default value if not found
-     * @tparam T the type that we expect
-     * @param name the name of the setting
-     * @param defaultValue the default value the setting is to be set to if it is non-existent
-     * @return the value of the setting specified by name
-     */
+    class File {
+    public:
+        /**
+         * @brief Loads the Config from the specified file, invalid values will be ignored (set to default value) but setting keys will still be registered
+         * @param path the absolute path to the config file
+         */
+        void Parse(const std::filesystem::path path);
+        /**
+         * @brief Saves this Config to the specified file
+         * @param path the absolute path to the file this Config is to be saved to
+         */
+        void Save(const std::filesystem::path path);
+        /**
+         * @brief Tries to get the specified setting, set it to the default value if not found
+         * @tparam T the type that we expect
+         * @param name the name of the setting
+         * @param defaultValue the default value the setting is to be set to if it is non-existent
+         * @param index the index to query, default is zero
+         * @return the value of the setting specified by name
+         */
+        template <typename T>
+        T Get(const std::string& name, const T defaultValue, const size_t index = 0);
+        /**
+         * @brief Sets the specified setting to the specified value
+         * @tparam T the type of this setting
+         * @param name the name of the setting to set
+         * @param value the value the setting is to be set to
+         * @param index the index to set, default is zero
+         * @return the new value of the setting specified by name
+         */
+        template <typename T>
+        T Set(const std::string& name, const T value, const size_t index = 0);
+
+    private:
+        typedef std::variant<double, std::string> value_type;
+        std::map<std::string, std::vector<value_type>> _configMap;
+    };
+
+    File& Global();
+
+    template <>
+    inline float File::Get<float>(const std::string& name, const float defaultValue, const size_t index)
+    {
+        return float(Get<double>(name, double(defaultValue), index));
+    }
+
+    template <>
+    inline short File::Get<short>(const std::string& name, const short defaultValue, const size_t index)
+    {
+        return short(Get<double>(name, double(defaultValue), index));
+    }
+
+    template <>
+    inline unsigned short File::Get<unsigned short>(const std::string& name, const unsigned short defaultValue, const size_t index)
+    {
+        return unsigned short(Get<double>(name, double(defaultValue), index));
+    }
+
+    template <>
+    inline int File::Get<int>(const std::string& name, const int defaultValue, const size_t index)
+    {
+        return int(Get<double>(name, double(defaultValue), index));
+    }
+
+    template <>
+    inline unsigned File::Get<unsigned>(const std::string& name, const unsigned defaultValue, const size_t index)
+    {
+        return unsigned(Get<double>(name, double(defaultValue), index));
+    }
+
     template <typename T>
-    T Get(const std::string& name, const T defaultValue);
-    /**
-     * @brief Sets the specified setting to the specified value
-     * @tparam T the type of this setting
-     * @param name the name of the setting to set
-     * @param value the value the setting is to be set to
-     * @return the new value of the setting specified by name
-     */
+    inline T File::Get(const std::string& name, const T defaultValue, const size_t index)
+    {
+        auto it = _configMap.find(name);
+        if (it != _configMap.end()) {
+            auto& values = it->second;
+            values.resize((std::max)(index, values.size()));
+            return std::get<T>(values.at(index));
+        }
+        else return File::Set(name, defaultValue, index);
+    }
+
     template <typename T>
-    T Set(const std::string& name, const T value);
-
-private:
-    std::map<std::string, std::variant<float, std::string>> _configMap;
-};
-
-File& Global();
-
-template <>
-inline int File::Get<int>(const std::string& name, const int defaultValue)
-{
-    return Get<float>(name, defaultValue);
-}
-
-template <>
-inline unsigned File::Get<unsigned>(const std::string& name, const unsigned defaultValue)
-{
-    return Get<float>(name, defaultValue);
-}
-
-template <typename T>
-inline T File::Get(const std::string& name, const T defaultValue)
-{
-    auto it = _configMap.find(name);
-    if (it != _configMap.end())
-        return std::get<T>((*it).second);
-    else
-        return File::Set(name, defaultValue);
-}
-
-template <typename T>
-inline T File::Set(const std::string& name, const T value)
-{
-    return std::get<T>(_configMap[name] = value);
-}
+    inline T File::Set(const std::string& name, const T value, const size_t index)
+    {
+        auto& values = _configMap[name];
+        values.resize((std::max)(index, values.size()));
+        return std::get<T>(values.at(index) = value);
+    }
 }
--- src/Config.cpp
+++ src/Config.cpp
@@ -1,60 +1,58 @@
 #include <Config.hpp>
 
-#include <exception>
 #include <filesystem>
 #include <fstream>
 #include <iterator>
 #include <sstream>
-#include <stdio.h> // for sscanf, fgets, fopen
-#include <vector>
 
 namespace Config {
-void File::Parse(const std::filesystem::path path)
-{
-    std::ifstream configFile(path);
-    for (std::string line; std::getline(configFile, line);) {
-        std::istringstream iss(line);
-        std::vector<std::string> words(
-            std::istream_iterator<std::string> { iss },
-            std::istream_iterator<std::string>());
-        if (words.size() > 2 && words.at(1) == "=") {
-            switch (words.size() - 2) {
-            case 1:
-                try {
-                    auto value{ std::stof(words.at(2)) };
-                    _configMap[words.at(0)] = value;
+    void File::Parse(const std::filesystem::path path)
+    {
+        std::ifstream configFile(path);
+        for (std::string line; std::getline(configFile, line);) {
+            std::istringstream iss(line);
+            std::vector<std::string> words(
+                std::istream_iterator<std::string> { iss },
+                std::istream_iterator<std::string>());
+            if (words.size() > 2 && words.at(1) == "=") {
+                size_t i = 2;
+                while (i < words.size()) {
+                    try {
+                        auto value{ std::stof(words.at(2)) };
+                        _configMap[words.at(0)].push_back(value);
+                    }
+                    catch (std::exception&) {
+                        _configMap[words.at(0)].push_back(words.at(2));
+                    }
                 }
-                catch (std::exception&) {
-                    _configMap[words.at(0)] = words.at(2);
-                }
-                break;
             }
         }
     }
-}
 
-void File::Save(const std::filesystem::path path)
-{
-    std::ofstream configFile;
-    configFile.open(path);
-    for (const auto& v : _configMap) {
-        configFile << v.first << " = ";
-        switch (v.second.index()) {
-        case 0:
-            configFile << std::get<0>(v.second);
-            break;
-        case 1:
-            configFile << std::get<1>(v.second);
-            break;
+    void File::Save(const std::filesystem::path path)
+    {
+        std::ofstream configFile;
+        configFile.open(path);
+        for (const auto& v : _configMap) {
+            configFile << v.first << " =";
+            for (const auto& value : v.second) {
+                switch (value.index()) {
+                case 0:
+                    configFile << ' ' << std::get<0>(value);
+                    break;
+                case 1:
+                    configFile << ' ' << std::get<1>(value);
+                    break;
+                }
+            }
+            configFile << '\n';
         }
-        configFile << '\n';
     }
-}
 
-File& Config::Global()
-{
-    static Config::File instance;
-    return instance;
-}
+    File& Config::Global()
+    {
+        static Config::File instance;
+        return instance;
+    }
 
 }
