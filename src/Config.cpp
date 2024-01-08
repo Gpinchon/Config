828a7f57e72fedf233db047da6b674fce68a57eb
gpinchon <pinchon.guillaume@gmail.com>
2024-01-08 21:32:52 +0100

    fix GCC compilation

--- src/Config.cpp
+++ src/Config.cpp
@@ -5,23 +5,29 @@
 #include <iterator>
 #include <sstream>
 
-namespace Config {
+namespace Config
+{
     void File::Parse(const std::filesystem::path path)
     {
         std::ifstream configFile(path);
-        for (std::string line; std::getline(configFile, line);) {
+        for (std::string line; std::getline(configFile, line);)
+        {
             std::istringstream iss(line);
             std::vector<std::string> words(
-                std::istream_iterator<std::string> { iss },
+                std::istream_iterator<std::string>{iss},
                 std::istream_iterator<std::string>());
-            if (words.size() > 2 && words.at(1) == "=") {
+            if (words.size() > 2 && words.at(1) == "=")
+            {
                 size_t i = 2;
-                while (i < words.size()) {
-                    try {
-                        auto value{ std::stof(words.at(2)) };
+                while (i < words.size())
+                {
+                    try
+                    {
+                        auto value{std::stof(words.at(2))};
                         _configMap[words.at(0)].push_back(value);
                     }
-                    catch (std::exception&) {
+                    catch (std::exception &)
+                    {
                         _configMap[words.at(0)].push_back(words.at(2));
                     }
                 }
@@ -33,10 +39,13 @@ namespace Config {
     {
         std::ofstream configFile;
         configFile.open(path);
-        for (const auto& v : _configMap) {
+        for (const auto &v : _configMap)
+        {
             configFile << v.first << " =";
-            for (const auto& value : v.second) {
-                switch (value.index()) {
+            for (const auto &value : v.second)
+            {
+                switch (value.index())
+                {
                 case 0:
                     configFile << ' ' << std::get<0>(value);
                     break;
@@ -49,10 +58,9 @@ namespace Config {
         }
     }
 
-    File& Config::Global()
+    File &Global()
     {
         static Config::File instance;
         return instance;
     }
-
 }
