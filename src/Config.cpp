77852cef5a8b923a86253fc1edf580c81d024ba4
gpinchon <pinchon.guillaume@gmail.com>
2024-01-10 01:13:27 +0100

    fix parsing

--- src/Config.cpp
+++ src/Config.cpp
@@ -18,16 +18,18 @@ namespace Config
                 std::istream_iterator<std::string>());
             if (words.size() > 2 && words.at(1) == "=")
             {
+                auto &key = _configMap[words.at(0)];
+                key.clear();
                 for (auto i = 2u; i < words.size(); ++i)
                 {
                     try
                     {
                         auto value{std::stof(words.at(i))};
-                        _configMap[words.at(0)].emplace_back(value);
+                        key.emplace_back(value);
                     }
                     catch (std::exception &)
                     {
-                        _configMap[words.at(0)].emplace_back(words.at(i));
+                        key.emplace_back(words.at(i));
                     }
                 }
             }
